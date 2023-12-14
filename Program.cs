29053eab1da66abd7b25dfe62dd36ddd1b2b6b4c
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2023-12-14 13:33:51 +0500

    Add Calculation Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter values: ");
+            Console.Write("First number: ");
+            string firstUerValues = Console.ReadLine();
+            Console.Write("Second number: ");
+            string secondUserValues=Console.ReadLine();
+
+            Console.WriteLine("Converting values in progress...");
+            decimal firstNumber = Convert.ToDecimal(firstUerValues);
+            decimal secondNumber = Convert.ToDecimal(secondUserValues);
+
+            Console.WriteLine("Here are your results: "); 
+            Console.WriteLine($"{firstNumber}+{secondNumber}={firstNumber+secondNumber}");
+        }
+    }
+}
+
